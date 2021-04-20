05d0a9df747e7b69379650133fb6db6ec0f44408
Erick Njuki <njukierick@gmail.com>
2021-04-21 00:48:50 +0300

    First commit

---
+++ src/actions.js
@@ -0,0 +1,6 @@
+export const DECREASE = "DECREASE";
+export const INCREASE = "INCREASE";
+export const REMOVE = "REMOVE";
+export const CLEAR_CART = "CLEAR_CART";
+export const GET_TOTALS = "GET_TOTALS";
+export const TOGGLE_AMOUNT = "TOGGLE_AMOUNT";
